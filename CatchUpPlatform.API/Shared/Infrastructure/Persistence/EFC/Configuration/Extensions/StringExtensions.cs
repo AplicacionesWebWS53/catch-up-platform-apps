642c633788a6af87f3efebcbc9c5afde5337e015
Al3xiel <gabriel280104casas@gmail.com>
2024-10-17 16:22:56 -0500

    feat(ors): added extensions for naming conventions.

--- CatchUpPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/Extensions/StringExtensions.cs
+++ CatchUpPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/Extensions/StringExtensions.cs
@@ -1,7 +1,21 @@
+using Humanizer;
+
 namespace CatchUpPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
+
+/// <summary>
+/// String Extensions
+/// </summary>
+/// <remarks>
+/// This class contains extension methods for string
+/// </remarks>
 
 public static class StringExtensions
 {
+    /// <summary>
+    /// Convert string to snake case
+    /// </summary>
+    /// <param name="text">The string to convert</param>
+    /// <returns>The string converted to snake case</returns>
     public static string ToSnakeCase(this string text)
     {
         return new string(Convert(text.GetEnumerator()).ToArray());
@@ -24,5 +38,15 @@ public static class StringExtensions
                 }
         }
     }
-    
+
+    /// <summary>
+    /// Convert string to plural
+    /// </summary>
+    ///
+    /// <param name="text">The string to convert</param>
+    /// <returns>The string converted to plural</returns>
+    public static string ToPlural(this string text)
+    {
+        return text.Pluralize(false);
+    }
 }
