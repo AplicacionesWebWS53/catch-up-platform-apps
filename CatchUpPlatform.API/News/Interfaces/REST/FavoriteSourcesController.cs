b2377fed1ad4d220f12a91b9d856a9d18935f140
Al3xiel <gabriel280104casas@gmail.com>
2024-10-17 16:54:20 -0500

    feat(favorite-source) add inbound service.

---
+++ CatchUpPlatform.API/News/Interfaces/REST/FavoriteSourcesController.cs
@@ -0,0 +1,46 @@
+using System.Net.Mime;
+using CatchUpPlatform.API.News.Domain.Model.Queries;
+using CatchUpPlatform.API.News.Domain.Services;
+using CatchUpPlatform.API.News.Interfaces.REST.Resources;
+using CatchUpPlatform.API.News.Interfaces.REST.Transform;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace CatchUpPlatform.API.News.Interfaces.REST;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+[Tags("Favorite Sources")]
+
+public class FavoriteSourcesController (
+    IFavoriteSourceCommandService favoriteSourceCommandService,
+    IFavoriteSourceQueryService favoriteSourceQueryService
+    ): ControllerBase
+{
+    /// <summary>
+    /// Get Favorite Sources by ID
+    /// </summary>
+    /// <param name="id">The Favorite Source ID provided by this API</param>
+    /// <returns>
+    /// A FavoriteSourceResource object
+    /// </returns>
+    [SwaggerOperation(
+        Summary = "Get Favorite Source by ID",
+        Description = "Get a Favorite Source Resource by given ID",
+        OperationId = "GetFavoriteSourceById")]
+    [SwaggerResponse(200, "The Favorite Source Resource was found", typeof(FavoriteSourceResource))]
+    [SwaggerResponse(404, "The Favorite Source Resource was not found")]
+    [HttpGet("{id}")]
+    public async Task<ActionResult> GetFavoriteSourceById(int id)
+    {
+        var getFavoriteSourceByIdQuery = new GetFavoriteSourceByIdQuery(id);
+        var result = await favoriteSourceQueryService.Handle(getFavoriteSourceByIdQuery);
+        if(result is null)
+        {
+            return NotFound();
+        }
+        var resource = FavoriteSourceResourceFromEntityAssembler.ToResourceFromEntity(result);
+        return Ok(result);
+    }
+}
